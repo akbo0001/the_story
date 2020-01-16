745517698193380e49e6b042964362b645ca7863
akbo0001 <akbo0001.algonquinlive.com>
2020-01-16 17:41:18 -0500

    adds script file

---
+++ script.js
@@ -0,0 +1,41 @@
+// Part1
+// console.log('hello world')
+
+// let colors = ['red', 'green', 'blue']
+// console.log(colors) 
+
+// colors.push('black')
+// console.log(colors)
+
+// console.log(colors[2])
+
+// console.log(colors.join(' '))
+
+// Part2
+// let student = {
+//     name: 'Bob',
+//     age: '1',
+//     isActive: true,
+// }
+
+// console.log(student)
+
+// student.classList = ['science', 'calculus', 'history']
+// console.log(student.classList[1])
+
+// Part3
+let story = {
+  verb: ['become', 'begin'],
+  adjective: ['calm', 'adjective'],
+  noun: 'garden'
+}
+
+console.log(`The sun did not ${story.verb[0]}`)
+It was too ${ story.adjective[0] } to ${ story.verb[1] }
+So we sat in the ${ story.noun[0] }
+All that ${ story.adjective[1] }, ${ story.adjective[1] }, ${ story.adjective[0] }.
+
+// The sun did not [verb 1]
+// It was too [adjective 1] to [verb 2]. 
+// So we sat in the [noun]
+// All that [adjective 2], [adjective 2], [adjective 1] day.
